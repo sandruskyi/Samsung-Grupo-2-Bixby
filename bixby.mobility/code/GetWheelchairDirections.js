8cfa3dead31f4c827ca847fd46ffaf3ac94c9736
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2020-06-16 21:57:17 +0200

    Api Openroute service integrada

--- bixby.mobility/code/GetWheelchairDirections.js
+++ bixby.mobility/code/GetWheelchairDirections.js
@@ -1,4 +1,15 @@
+var http = require('http')
+var console = require('console')
+var config = require('config')
+
 module.exports.function = function getWheelchairDirections (sourcePoint, destinationPoint) {
+  // La siguiente línea estaría bien si openroute estuviera implementado no solo para el centro de Madrid
+  //var response = http.getUrl(config.get('urlOpenroute') + '?api_key='+ config.get('keyOpenroute') + '&start='+ sourcePoint.point.latitude+','+ sourcePoint.point.longitude+'&end='+ destinationPoint.point.latitude+','+ destinationPoint.point.longitude , { format: 'json' });
+  
+  // Funcionalidad de muestra desde el punto "Parque del Retiro": 40.415751, -3.688703 
+  var response = http.getUrl(config.get('urlOpenroute') + '?api_key='+ config.get('keyOpenroute') + '&start=-3.68705,40.418332&end='+ destinationPoint.point.longitude +','+ destinationPoint.point.latitude , { format: 'json' });
+
+  console.log(response);
   return {
     sourcePoint: sourcePoint, 
     destinationPoint: destinationPoint
