e5e1abd3f8cc4ccb0be9589c5ab7feb2f0bead18
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2020-06-18 22:05:08 +0200

    Modelo y js creados || OpenRouteService

--- bixby.mobility/code/GetWheelchairDirections.js
+++ bixby.mobility/code/GetWheelchairDirections.js
@@ -10,8 +10,25 @@ module.exports.function = function getWheelchairDirections (sourcePoint, destina
   var response = http.getUrl(config.get('urlOpenroute') + '?api_key='+ config.get('keyOpenroute') + '&start=-3.68705,40.418332&end='+ destinationPoint.point.longitude +','+ destinationPoint.point.latitude , { format: 'json' });
 
   console.log(response);
+  console.log(response.features);
+  console.log(response.features[0].properties.segments[0].steps);
+  steps = response.features[0].properties.segments[0].steps;
+  console.log(response.features[0].properties.summary);
+  summary = response.features[0].properties.summary;
+  console.log(response.features[0].geometry);
+  geometry = response.features[0].geometry.coordinates;
+  console.log("OK " + geometry.length);
+  newCoordinates = new Array(geometry.length);
+  for(var i = 0; i < geometry.length; i++){
+    newCoordinates[i] = {latitude:geometry[i][1],longitude:geometry[i][0]};
+  }
+
+  console.log(newCoordinates);
   return {
     sourcePoint: sourcePoint, 
-    destinationPoint: destinationPoint
-  };
+    destinationPoint: destinationPoint,
+    steps: steps,
+    summary : summary,
+    newCoordinates : newCoordinates
+  }
 }
