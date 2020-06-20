8394bf6026ecfdbd2e7a4c734e7b30bf3b3b2d7a
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2020-06-20 20:28:23 +0200

    Direcciones indicadas en pantalla y REGEX al Castellano || OpenRouteService

--- bixby.mobility/code/GetWheelchairDirections.js
+++ bixby.mobility/code/GetWheelchairDirections.js
@@ -23,6 +23,32 @@ module.exports.function = function getWheelchairDirections (sourcePoint, destina
   for(var i = 0; i < geometry.length; i++){
     newCoordinates[i] = {latitude:geometry[i][1],longitude:geometry[i][0]};
   }
+  for(var i=0; i < steps.length; i++){
+    //if(steps[i].instruction == 'Head north'){
+    //  steps[i].instruction = 'Ve hacia el Norte';
+    //}else if(steps[i].instruction == 'Head north'){
+    
+    //REGEX: 
+    steps[i].instruction = steps[i].instruction.replace(/Head north/gi, 'Dirígete hacia el Norte');
+    steps[i].instruction = steps[i].instruction.replace(/Head south/gi, 'Dirígete hacia el Sur');
+    steps[i].instruction = steps[i].instruction.replace(/Head east/gi, 'Dirígete hacia el Este');
+    steps[i].instruction = steps[i].instruction.replace(/Head west/gi, 'Dirígete hacia el Oeste');
+    steps[i].instruction = steps[i].instruction.replace(/Turn left onto/gi, 'Gire a la izquierda en');
+    steps[i].instruction = steps[i].instruction.replace(/Turn right onto/gi, 'Gire a la derecha en');
+    steps[i].instruction = steps[i].instruction.replace(/Turn left/gi, 'Gire a la izquierda');
+    steps[i].instruction = steps[i].instruction.replace(/Turn right/gi, 'Gire a la derecha');
+    steps[i].instruction = steps[i].instruction.replace(/Turn slight left/gi, 'Gire levemente a la izquierda');
+    steps[i].instruction = steps[i].instruction.replace(/Turn slight right/gi, 'Gire levemente a la derecha');
+    steps[i].instruction = steps[i].instruction.replace(/Turn sharp left/gi, 'Gire bruscamente a la izquierda');
+    steps[i].instruction = steps[i].instruction.replace(/Turn sharp right/gi, 'Gire bruscamente a la derecha');
+    steps[i].instruction = steps[i].instruction.replace(/Keep left/gi, 'Manténgase a la izquierda');
+    steps[i].instruction = steps[i].instruction.replace(/Keep right/gi, 'Manténgase a la derecha');
+    steps[i].instruction = steps[i].instruction.replace(/Continue straight onto/gi, 'Continúa recto hacia');
+    steps[i].instruction = steps[i].instruction.replace(/Arrive at your destination, on the left/gi, 'Llegue a su destino, a la izquierda');
+    steps[i].instruction = steps[i].instruction.replace(/Arrive at your destination, on the right/gi, 'Llegue a su destino, a la derecha');
+    steps[i].instruction = steps[i].instruction.replace(/Arrive at your destination/gi, 'Llegue a su destino');
+    steps[i].instruction = steps[i].instruction.replace(/onto/gi, 'en');
+  }
 
   console.log(newCoordinates);
   return {
