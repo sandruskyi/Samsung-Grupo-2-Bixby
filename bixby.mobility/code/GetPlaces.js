0df674357069fe09f27c553ae81680c34b75b058
Sandrus <36504512+sandruskyi@users.noreply.github.com>
2020-06-22 23:16:52 +0200

    Código, Modelos y endpoints funcionando perfectos || Places

--- bixby.mobility/code/GetPlaces.js
+++ bixby.mobility/code/GetPlaces.js
@@ -5,10 +5,12 @@ var config = require('config')
 module.exports.function = function getPlaces (sourcePoint) {
   var response = http.getUrl(config.get('WHEurlLugaresCercanos') + '?appToken='+ config.get('WHEtoken') + '&latitude=' +sourcePoint.point.latitude +'&longitude='+ sourcePoint.point.longitude +'&accuracy=1000&includePlacesWithoutAccessibility=1' , { format: 'json' });
   console.log(response)
-  features = response.features
-  console.log(features)
+  features2 = response.features
+  console.log(features2)
+  //features2 = features[0]
+  //console.log(features2)
   return {
     sourcePoint: sourcePoint,
-    features : features
+    features2 : features2
   } 
 }
